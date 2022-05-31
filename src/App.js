4ac732a66dd9b7163a9462e33a3388a7ccf77d74
Ghassan Dabak <ghassan.h.dabak@gmail.com>
2022-05-31 15:49:33 +0300

    filter and search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import { BiCalendar, BiCheck } from "react-icons/bi";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState("petName");
+  const [orderBy, setOrderBy] = useState("");
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((elem) => {
+        setData(elem);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const handleSearch = data
+    ?.filter((item) => {
+      return (
+        item.petName.toLowerCase().includes(search.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(search.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(search.toLowerCase())
+      );
+    })
+    .sort((a, b) => {
+      const order = orderBy === "asc" ? 1 : -1;
+      return a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order
+        : 1 * order;
+    });
+
+  const deleteApp = (id) => {
+    setData(data.filter((elem) => elem.id !== id));
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>
+          <BiCalendar /> Appointments
+        </h1>
+        <hr />
+        <input type="text" value={search} onChange={(e) => handleChange(e)} />
+        <hr />
+      </div>
+      <div>
+        <ul>
+          <li onClick={()=>setSortBy('petName')}>petname {(sortBy === 'petName') && <BiCheck />}</li>
+          <li onClick={()=>setSortBy('ownerName')}>ownerName {(sortBy === 'ownerName') && <BiCheck />}</li>
+          <li onClick={()=>setSortBy('aptDate')}>aptDate {(sortBy === 'aptDate') && <BiCheck />}</li>
+          <li onClick={()=>setOrderBy('asc')}>asc {(orderBy === 'asc') && <BiCheck />}</li>
+          <li onClick={()=>setOrderBy('desc')}>desc {(orderBy === 'desc') && <BiCheck />}</li>
+        </ul>
+      </div>
+      <div>
+        {handleSearch.length > 0 ? (
+          handleSearch.map((elem, index) => {
+            return (
+              <div key={elem.id}>
+                <h1>
+                  {index + 1}-{elem.petName}
+                </h1>
+                <h3>{elem.ownerName}</h3>
+                <h4>{elem.aptNotes}</h4>
+                <h5>{elem.aptDate}</h5>
+                <p onClick={() => deleteApp(elem.id)}>X</p>
+              </div>
+            );
+          })
+        ) : (
+          <p>no match</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
