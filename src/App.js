303054a47ef7f0b9211aafee52fb9180c2f7cfd3
Ghassan Dabak <ghassan.h.dabak@gmail.com>
2022-08-16 22:22:07 +0300

    added clear from data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import { useCallback, useEffect, useState } from "react";
 import { BiCalendar, BiCheck } from "react-icons/bi";
 
 const App = () => {
+  const clearData = {
+    ownerName:'',
+    petName:"",
+    aptDate:'',
+    aptTime:'',
+    aptNotes:''
+    }
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [sortBy, setSortBy] = useState("petName");
   const [orderBy, setOrderBy] = useState("");
+  const [formData,setFormData] = useState(clearData)
+  const [showForm, setShowForm] = useState(false);
 
   const fetchData = useCallback(() => {
     fetch("./data.json")
@@ -19,8 +28,12 @@ const App = () => {
     fetchData();
   }, [fetchData]);
 
-  const handleSearch = data
-    ?.filter((item) => {
+  const handleChange = (e)=>{
+    const {name,value} = e.target
+    setFormData({...formData, [name]:value})
+  }
+
+  const handleSearch = data?.filter((item) => {
       return (
         item.petName.toLowerCase().includes(search.toLowerCase()) ||
         item.ownerName.toLowerCase().includes(search.toLowerCase()) ||
@@ -38,7 +51,7 @@ const App = () => {
     setData(data.filter((elem) => elem.id !== id));
   };
 
-  const handleChange = (e) => {
+  const handleSearchChange = (e) => {
     setSearch(e.target.value);
   };
 
@@ -48,17 +61,43 @@ const App = () => {
         <h1>
           <BiCalendar /> Appointments
         </h1>
+        <button onClick={() => setShowForm(!showForm)}>Add Appointment</button>
+        <hr/>
+        {showForm && (
+          <form>
+            <label>Owner Name</label>
+            <input type="text" name="ownerName" onChange={handleChange} value={formData.ownerName}/>
+            <label>Pet Name</label>
+            <input type="text" name="petName" onChange={handleChange} value={formData.petName}/>
+            <label>Apt Date</label>
+            <input type="date" name="aptDate" onChange={handleChange} value={formData.aptDate}/>
+            <label>Apt Time</label>
+            <input type="time" name="aptTime" onChange={handleChange} value={formData.aptTime}/>
+            <label>Apt Notes</label>
+            <input type="text" name="aptNotes" onChange={handleChange} value={formData.aptNotes}/>
+          </form>
+        )}
         <hr />
-        <input type="text" value={search} onChange={(e) => handleChange(e)} />
+        <input type="text" value={search} onChange={(e) => handleSearchChange(e)} />
         <hr />
       </div>
       <div>
         <ul>
-          <li onClick={()=>setSortBy('petName')}>petname {(sortBy === 'petName') && <BiCheck />}</li>
-          <li onClick={()=>setSortBy('ownerName')}>ownerName {(sortBy === 'ownerName') && <BiCheck />}</li>
-          <li onClick={()=>setSortBy('aptDate')}>aptDate {(sortBy === 'aptDate') && <BiCheck />}</li>
-          <li onClick={()=>setOrderBy('asc')}>asc {(orderBy === 'asc') && <BiCheck />}</li>
-          <li onClick={()=>setOrderBy('desc')}>desc {(orderBy === 'desc') && <BiCheck />}</li>
+          <li onClick={() => setSortBy("petName")}>
+            petname {sortBy === "petName" && <BiCheck />}
+          </li>
+          <li onClick={() => setSortBy("ownerName")}>
+            ownerName {sortBy === "ownerName" && <BiCheck />}
+          </li>
+          <li onClick={() => setSortBy("aptDate")}>
+            aptDate {sortBy === "aptDate" && <BiCheck />}
+          </li>
+          <li onClick={() => setOrderBy("asc")}>
+            asc {orderBy === "asc" && <BiCheck />}
+          </li>
+          <li onClick={() => setOrderBy("desc")}>
+            desc {orderBy === "desc" && <BiCheck />}
+          </li>
         </ul>
       </div>
       <div>
